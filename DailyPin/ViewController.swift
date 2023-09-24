a0d85f6b3e54d4bc001929a4fcd630aa37fccde6
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-09-24 17:54:25 +0900

    Initial Commit

---
+++ DailyPin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyPin
+//
+//  Created by 김지연 on 2023/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
