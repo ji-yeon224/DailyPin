bea81eb40f82a6073c5550926a470b31f6fc53b0
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2023-09-26 15:08:02 +0900

    [FEAT] 컬러 셋 추가, 다국어 파일 생성

---
+++ DailyPin/Utility/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  DailyPin
+//
+//  Created by 김지연 on 2023/09/25.
+//
+
+import UIKit
+
+enum Color {
+    
+    static let background = UIColor(named: "backGroundColor")
+    static let basicText = UIColor(named: "textColor")
+    static let mainColor = UIColor(named: "mainColor")
+    
+}
+
+
