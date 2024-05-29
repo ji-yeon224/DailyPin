4af00c8c7805d2bf7c9a390c3947c32fb66ed868
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-05-29 11:53:36 +0900

    코드 정리

--- DailyPin/Scene/Main/AlertMapViewController.swift
+++ DailyPin/Scene/Main/AlertMapViewController.swift
@@ -18,32 +18,33 @@ final class AlertMapViewController : UIViewController{
         }
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-  override func viewDidLoad() {
-      super.viewDidLoad()
-      // 뷰 컨트롤러에 맵킷 뷰를 추가한다.
-      let mapKitView = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-      self.view = mapKitView
-      self.preferredContentSize.height = 200
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 뷰 컨트롤러에 맵킷 뷰를 추가한다.
+        let mapKitView = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        self.view = mapKitView
+        self.preferredContentSize.height = 200
         
-      //표시할 위치
-      let pos = CLLocationCoordinate2D(latitude: cood.latitude, longitude: cood.longitude)
+        //표시할 위치
+        let pos = CLLocationCoordinate2D(latitude: cood.latitude, longitude: cood.longitude)
         
-      let span = MKCoordinateSpan(latitudeDelta: 0.0025, longitudeDelta: 0.0025)
+        let span = MKCoordinateSpan(latitudeDelta: 0.0025, longitudeDelta: 0.0025)
         
-      //3.지도 영역을 정의
-      let region = MKCoordinateRegion(center: pos, span: span)
+        //3.지도 영역을 정의
+        let region = MKCoordinateRegion(center: pos, span: span)
         
-      //4.지도 뷰에 표시
-      mapKitView.region = region
-      mapKitView.regionThatFits(region)
+        //4.지도 뷰에 표시
+        mapKitView.region = region
+        mapKitView.regionThatFits(region)
         
-      //5.위치를 핀으로 표시
-      let point = MKPointAnnotation()
-      point.coordinate = pos
-      mapKitView.addAnnotation(point)
-  }
+        //5.위치를 핀으로 표시
+        let point = MKPointAnnotation()
+        point.coordinate = pos
+        mapKitView.addAnnotation(point)
+    }
 }
